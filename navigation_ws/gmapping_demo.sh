86d6c18add430eba24dddd80dbd49370a458caad
Aleksey Parfyonov <kantengri3@gmail.com>
2020-03-12 04:59:24 +0300

    move_base with odom root

---
+++ navigation_ws/gmapping_demo.sh
@@ -0,0 +1,6 @@
+export TURTLEBOT_3D_SENSOR=kinect
+roslaunch my_navigation gmapping_demo.launch &
+roslaunch my_navigation amcl_demo.launch
+
+# roslaunch my_navigation 2dnav_ros_tr.launch
+# roslaunch my_navigation graveyard.launch
